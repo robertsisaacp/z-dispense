3682e140ad7689eab26bbed166703062b1fe3075
robertsisaacp <robertsisaacp@gmail.com>
2018-04-01 18:47:59 -0500

    rewrite of websocket

--- raspi_server/index.js
+++ raspi_server/index.js
@@ -1,48 +1,37 @@
 'use strict';
 
-/*
- * Third party
- */
-
 const fs = require('fs');
 const WebSocketServer = require('ws').Server;
 const WebSocket = require('ws');
-/*
- * Load all modules to array
- */
-//const modules = fs
-//    .readdirSync(MODULES_DIR)
-//    .map(item => require(MODULES_DIR + item));
 
-const connections = {};
+//const connections = {};
 
-/*
- * Broadcast key-value pair to all connections
- */
-function broadcast (key, value) {
-    for (let id in connections) {
-        connections[id].send(
-            JSON.stringify({
-                key,
-                value
-            })
-        );
-    }
-}
 
-function onNewConnection (socket) {
-    let isAuthenticated = true;
-    const id = Math.random();
-    connections[id] = socket;
 
-    socket.on('message', (data) => {
-        try {
-            data = JSON.parse(data);
-            console.logl
-        } catch (e){
-            data = {};
-        }
-
+//function broadcast (key, value) {
+//    for (let id in connections) {
+//        connections[id].send(
+//            JSON.stringify({
+//                key,
+//                value
+//            })
+//        );
+//    }
+//}
+//
+//function onNewConnection (socket) {
+//    let isAuthenticated = true;
+//    const id = Math.random();
+//    connections[id] = socket;
+//
+//    socket.on('message', (data) => {
+//        try {
+//            data = JSON.parse(data);
+//            console.logl
+//        } catch (e){
+//            data = {};
+//        }
+//
         // disconnect client if the first message is not valid auth token
 //        if (!isAuthenticated && data.auth !== homeConfig.authToken) {
 //            socket.close();
@@ -66,27 +55,27 @@ function onNewConnection (socket) {
  //           )
  //       );
 
-        if (data.key === 'STATUS') {
-            setTimeout(() => {
-                broadcast('STATUS');
-            }, 250);
-        }
-    });
-
-    socket.on('close', () => {
-        // remove connection from the broadcast list
-        delete connections[id];
-    });
-}
-
-
-const wss = new WebSocketServer({ port: 3000 });
-
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
-
-  ws.send('back');
-});
+//        if (data.key === 'STATUS') {
+//            setTimeout(() => {
+//                broadcast('STATUS');
+//            }, 250);
+//        }
+//    });
+//
+//    socket.on('close', () => {
+//        // remove connection from the broadcast list
+//        delete connections[id];
+//    });
+//}
+//
+//
+//const wss = new WebSocketServer({ port: 3000 });
+//
+//wss.on('connection', function connection(ws) {
+//  ws.on('message', function incoming(message) {
+//    console.log('received: %s', message);
+//  });
+//
+//  ws.send('back');
+//});
 
