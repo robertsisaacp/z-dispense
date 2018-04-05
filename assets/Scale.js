962dfb77c67d2224f297551b9711b18c24097d04
Isaac <robertsisaacp@gmail.com>
2018-04-04 20:01:22 -0400

    fixed scale functions

--- assets/Scale.js
+++ assets/Scale.js
@@ -5,8 +5,8 @@ const { width, height } = Dimensions.get('window');
 const guidelineBaseWidth = 350;
 const guidelineBaseHeight = 680;
 
-const scale = size => width / guidelineBaseWidth * size;
-const scaleVertical = size => height / guidelineBaseHeight * size;
+const scale = (size) => width / guidelineBaseWidth * size;
+const scaleVertical = (size) => height / guidelineBaseHeight * size;
 const scaleModerate = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
 
 export {scale, scaleVertical, scaleModerate};
