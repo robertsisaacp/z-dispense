57c6d4aec358ccbafd27b949c9d6b9965f4add51
Isaac <robertsisaacp@gmail.com>
2018-04-07 18:02:46 -0400

    added test python script

--- raspi_server/index.js
+++ raspi_server/index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var spawn = require("child_process").spawn;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -15,3 +16,20 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+// Parameters passed in spawn -
+// 1. type_of_script
+// 2. list containing Path of the script
+//    and arguments for the script 
+ 
+// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+// so, first name = Mike and last name = Will
+var process = spawn('python',["./test.py",
+                        req.query.firstname,
+                        req.query.lastname] );
+
+// Takes stdout data from script which executed
+// with arguments and send this data to res object
+process.stdout.on('data', function(data) {
+    res.send(data.toString());
+} )
