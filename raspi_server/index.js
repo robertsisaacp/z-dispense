6851653c966db8e02c9e9c45aa1536c6f32cf4b7
Isaac <robertsisaacp@gmail.com>
2018-04-08 12:17:02 -0400

    updates

--- raspi_server/index.js
+++ raspi_server/index.js
@@ -1,4 +1,3 @@
-
 var express = require('express');
 var app = express();
 
@@ -11,19 +10,19 @@ app.listen(3000, function() {
 app.get('/params', callParams);
 
 function callParams(req, res) {
-  // using spawn instead of exec, prefer a stream over a buffer
-  // to avoid maxBuffer issue
-  console.log('test')
   var spawn = require('child_process').spawn;
-  var process = spawn('python3', 
-    ["./python/test.py",
+  var process = spawn('python', 
+    ["./python/gpio.py",
     req.query._prec,    // precision
     req.query._type,    // circle, line, ... 
     req.query._height  // in mm
   ]);
-  console.log("Height: " + req.query._height)
-  console.log("Type: " + req.query._type)
-  console.log("Precision: " + req.query._prec)
+
+  // testing
+  console.log("Height: " + req.query._height
+    + "Type: " + req.query._type
+    + "Precision: " + req.query._prec);
+
   process.stdout.on('data', function(data) {
     res.send(data.toString());
   });
