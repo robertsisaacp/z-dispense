9dd06370300fe3c7e77423d1029d1855221d6222
robertsisaacp <robertsisaacp@gmail.com>
2018-04-08 00:45:21 -0500

    starting rewrite for node to py interaction

--- raspi_server/index.js
+++ raspi_server/index.js
@@ -3,15 +3,17 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var spawn = require("child_process").spawn;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+app.get('/params', callParams);
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        console.log('message' + msg);
-    });
-});
+function callParams(req, res) {
+
+
+
+//io.on('connection', function(socket){
+//    socket.on('chat message', function(msg){
+//        console.log('message' + msg);
+//    });
+//});
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
@@ -24,7 +26,8 @@ http.listen(3000, function(){
  
 // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
 // so, first name = Mike and last name = Will
-var process = spawn('python', 'test.py')
+
+var process = spawn("python", ["./test.py"]);
 //var process = spawn('python',["./test.py",
                         //req.query.firstname,
                         //req.query.lastname] );
