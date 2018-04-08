474b41e5a834e60891c0b16d6a632e860f15a3d8
Isaac <robertsisaacp@gmail.com>
2018-04-08 02:06:16 -0400

    bridged javascript to python test script

--- raspi_server/index.js
+++ raspi_server/index.js
@@ -1,39 +1,23 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var spawn = require("child_process").spawn;
+// http://localhost:3000/params?prec=100&type=10&zHeight=100
+
+var express = require(‘express’);
+var app = express();
+app.listen(3000, function () {
+  console.log(‘server running on port 3000’);
+})
 
 app.get('/params', callParams);
 
 function callParams(req, res) {
-
-
-
-//io.on('connection', function(socket){
-//    socket.on('chat message', function(msg){
-//        console.log('message' + msg);
-//    });
-//});
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-// Parameters passed in spawn -
-// 1. type_of_script
-// 2. list containing Path of the script
-//    and arguments for the script 
- 
-// E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
-// so, first name = Mike and last name = Will
-
-var process = spawn("python", ["./test.py"]);
-//var process = spawn('python',["./test.py",
-                        //req.query.firstname,
-                        //req.query.lastname] );
-
-// Takes stdout data from script which executed
-// with arguments and send this data to res object
-process.stdout.on('data', function(data) {
-    //res.send(data.toString());
-} )
+  // using spawn instead of exec, prefer a stream over a buffer
+  // to avoid maxBuffer issue
+  var spawn = require(“child_process”).spawn;
+  var process = spawn('python’, [“./python/test.py”,
+    req.query.prec,    // precision
+    req.query.type,    // circle, line, ... 
+    req.query.zHeight, // in mm
+  ]);
+  process.stdout.on(‘data’, function (data) {
+    res.send(data.toString());
+  });
+}
--- raspi_server/python/test.py
+++ raspi_server/python/test.py
@@ -1,1 +1,8 @@
-print("hi! this is test.py")
+import sys
+
+prec = float(sys.argv[1])
+type = float(sys.argv[2])
+zHeight = float(sys.argv[3])
+
+print(str("" + prec + ", " + type + ", " + zHeight))
+sys.stdout.flush()
