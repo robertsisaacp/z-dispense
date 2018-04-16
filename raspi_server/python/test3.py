65b4a297b20a9590c8094595e2e772936d825e9f
Isaac <robertsisaacp@gmail.com>
2018-04-15 20:20:52 -0400

    int

--- raspi_server/python/test3.py
+++ raspi_server/python/test3.py
@@ -11,5 +11,5 @@ import sys
 typeDict = {0: "line", 1: "circle", 2: "square"}
 print("argv[1]: " + sys.argv[1])
 print("argv[2]: " + sys.argv[2])
-print("argv[2]: " + str(sys.argv[2]))
+print("argv[2]: " + typeDict[int(sys.argv[2])])
 print("argv[3]: " + sys.argv[3])
